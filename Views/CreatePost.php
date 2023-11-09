45c3b8cf9775dd65fcd77d56b54eaa905c91f585
LADebey <louisadriendebey@gmail.com>
2023-11-09 09:57:27 +0100

    2nd commit css/current_time

--- Views/CreatePost.php
+++ Views/CreatePost.php
@@ -4,26 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Créer une enchère/Mettre en vente</title>
+    <link rel="stylesheet" type="text/css" href="CreatePost.css"/>
 </head>
 <body>
  <!-- include ("Menu.php"); -->
+ <?php $currentDate = date('d-m-Y'); ?>
 
  <div class="CreatePostContainer">
     <form class="CreatePostForm" action="Post.php" method="post">
+        <h2>Créer une enchère</h2>
         <label for="model">Modèle de la voiture:</label>
         <input type="text" name="model" placeholder="Corolla"/>
         <label for="brand">Constructeur de la voiture:</label>
         <input type="text" name="brand" placeholder="Toyota"/>
         <label for="power">Puissance:</label>
         <input type="text" name="power" placeholder="140"/>
-        <label for="year">Année:</abel>
+        <label for="years">Année:</abel>
         <input type="number" name="year" placeholder="2023" min="1886" max="2024"placeholder="2023" />
-        <label for="description">Description:</label>
+        <label for="descriptions">Description:</label>
         <input type="textarea" name="description" placeholder="Toyota Corolla Toyota Corolla Toyota Corolla Toyota Corolla"/>
         <label for="min_price">Prix minimum:</label>
         <input type="number" name="min_price" step="0.01" min="1" placeholder="8000"/>
         <label for="date_end">Date de fin de l'enchère:</label>
-        <input type="date" name="date_end"/>
+        <input type="date" name="date_end" min="<?php echo $currentDate; ?>"/>
+        <input type="hidden" name="current_date" value="<?php echo $currentDate; ?>"/>
         <input type="submit"/>
     </form>
 
@@ -36,10 +40,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
         $_POST["model"], 
         $_POST["brand"], 
         $_POST["power"], 
-        $_POST["year"],
-        $_POST["description"],
+        $_POST["years"],
+        $_POST["descriptions"],
         $_POST["min_price"],
-        $_POST["date_end"]
+        $_POST["date_end"],
+        $_POST["current_date"]
         );
       $myAuction -> sauvegarde();
     }
