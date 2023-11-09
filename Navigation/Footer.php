76da7e9a49c30d9171da4aa37fd6bb1de15edaa3
LADebey <louisadriendebey@gmail.com>
2023-11-09 14:36:21 +0100

    Footer CGU contact us + css

---
+++ Navigation/Footer.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <link rel="stylesheet" type="text/css" href="Footer.css"/>
+</head>
+<body>
+
+<footer class="footer">
+<div class="smaller-column">
+    <img src="Images\VroomVroomBids.jpeg"/>
+    </div>
+    <div class="smaller-column">
+        <h2>Vroom<span class="dotcom">Vroom</span>Bids.com</h1>
+      <ul>
+        <li>À propos de nous</li>
+        <li><a href="mailto:contact@vroomvroombids.com">Nous contacter</a></li>
+        <li>Conditions générales d'utilisations</li>
+      </ul>
+    </div>
+    <div class="larger-column">
+    <p>© 2023 VroomVroomBids. Tous droits réservés. Aucune partie de ce site web ne peut être reproduite, distribuée, ou transmise sous quelque forme ou par quelque moyen que ce soit, y compris la photocopie, l'enregistrement, ou d'autres méthodes électroniques ou mécaniques, sans la permission écrite préalable de VroomVroomBids, sauf dans le cas de courtes citations intégrées à des critiques et certaines autres utilisations non commerciales autorisées par la loi sur le droit d'auteur. Pour toute demande de permission, veuillez contacter l'équipe VroomVroomBids via le site web.</p>
+    </div>
+ 
+  
+  </footer>
+    
+</body>
+</html>
