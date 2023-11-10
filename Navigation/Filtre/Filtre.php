f26c03ca7e3f9e284511bd6e17346fe30918ee29
LucGaluc <amartinoluc@gmail.com>
2023-11-10 14:47:33 +0100

    css filtre

--- Navigation/Filtre/Filtre.php
+++ Navigation/Filtre/Filtre.php
@@ -1,5 +1,5 @@
 <?php
-include("Navigation\Menu.php");
+include("../Navigation/Menu.php");
 
 try {
     $bdd = new PDO("mysql:host=127.0.0.1;port=8889;dbname=bocal_vroomvroombids", "root", "root");
