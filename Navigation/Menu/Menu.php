8b7e376513a97c197d96002a994deff29a0789d6
Dennys5 <dennys.rocca07@gmail.com>
2023-11-14 09:53:30 +0100

    deplacement du menu dans le dossier menu

--- Navigation/Menu/Menu.php
+++ Navigation/Menu/Menu.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/VroomVroombids/Navigation/Menu/Menu.css">
+    <link rel="stylesheet" href="../Navigation/Menu.css">
     <title>Document</title>
 </head>
 
@@ -12,16 +12,11 @@
 
 
     <div id="nav">
-
-        <a href="/VroomVroombids/Views/Home/Home.php"><img id='logo' src='https://cdn.discordapp.com/attachments/1171733145700282409/1171742236124389376/f43a35e4-54ac-4efe-ab61-97a08b23cfe5.jpeg?ex=655dc8ff&is=654b53ff&hm=fafae2e4bd3c319ad600160edf7bfd689bf543cb71be86d4afc8c8ae4316f182&' alt=''></a>
-        <div class='navCont'>
-            <a href="/VroomVroombids/Views/Post/Posts.php">Enchères</a>
-            <a href="/VroomVroombids/Views/Login/Login.php">Connexion</a>
-            <a href="/VroomVroombids/Views/Register/Register.php">Inscription</a>
-        </div>
-
-
+        <a href="/VroomVroombids/Views/Bids.php">Enchères</a>
+        <a href="/VroomVroombids/Views/Login/Login.php">Connexion</a>
+        <a href="/VroomVroombids/Views/Register/Register.php">Inscription</a>
     </div>
+    
 </body>
 
 </html>
